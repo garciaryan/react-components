8122172d92bc224b00dcb04c6241c6f5997e440d
Ryan Garcia <garciaryan007@yahoo.com>
2019-02-10 13:41:21 -0800

    props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail 
+        author="Sam" 
+        timeAgo="Today at 4:20pm" 
+        content="Nice post!" 
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail 
+        author="Jane" 
+        timeAgo="Today at 2:00am" 
+        content="This sucks dog"
+        avatar={faker.image.avatar()} 
+      />
+      <CommentDetail 
+        author="Alex" 
+        timeAgo="Yesterday at 3:33pm" 
+        content="Yes!"
+        avatar={faker.image.avatar()} 
+      />
     </div>
   );
 };
